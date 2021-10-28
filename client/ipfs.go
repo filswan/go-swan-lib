38728939e98dbebda78b147643751d2b376f8b33
DoraNebula <fchen@nbai.io>
2021-10-28 09:36:31 -0600

    Update ipfs.go

--- client/ipfs.go
+++ client/ipfs.go
@@ -54,5 +54,11 @@ func IpfsUploadCarFileByWebApi(apiUrl, carFilePath string) (*string, error) {
 	carFileHash := utils.GetFieldStrFromJson(response, "Hash")
 	logs.GetLogger().Info(carFileHash)
 
+	if carFileHash == constants.EMPTY_STRING {
+		err := fmt.Errorf("cannot get file hash from response:%s", response)
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
 	return &carFileHash, nil
 }
