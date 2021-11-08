e0092471ad9ea3a64bc3de94c8b37f020eebce75
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-11-08 12:24:18 -0600

    error message for more information

--- client/ipfs.go
+++ client/ipfs.go
@@ -2,41 +2,11 @@ package client
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/filswan/go-swan-lib/constants"
 	"github.com/filswan/go-swan-lib/logs"
 	"github.com/filswan/go-swan-lib/utils"
 )
-
-func IpfsUploadCarFile(carFilePath string) (*string, error) {
-	cmd := "ipfs add " + carFilePath
-	logs.GetLogger().Info(cmd)
-
-	result, err := ExecOsCmd(cmd, false)
-
-	if err != nil {
-		logs.GetLogger().Error(err)
-		return nil, err
-	}
-
-	if result == "" {
-		err := fmt.Errorf("cmd(%s) result is empty", cmd)
-		logs.GetLogger().Error(err)
-		return nil, err
-	}
-
-	words := strings.Fields(result)
-	if len(words) < 2 {
-		err := fmt.Errorf("cmd(%s) result(%s) does not have enough fields", cmd, result)
-		logs.GetLogger().Error(err)
-		return nil, err
-	}
-
-	carFileHash := words[1]
-
-	return &carFileHash, nil
-}
 
 func IpfsUploadCarFileByWebApi(apiUrl, carFilePath string) (*string, error) {
 	response, err := HttpUploadFileByStream(apiUrl, carFilePath)
